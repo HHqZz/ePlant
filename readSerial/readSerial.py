a89c9e4672f2a2b3c0c5e7c9641b1dcd0a3e2457
Alexandre <alexandre@localhost>
2020-01-18 17:39:49 -0500

    Modify the snippet to differentiate between values

--- readSerial/readSerial.py
+++ readSerial/readSerial.py
@@ -6,8 +6,13 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5) # open serial port with 5s timeout
 
 while(1):
-    print(ser.readline())
-    print(ser.readline())
-    print(ser.readline())
+    msg = ser.readline().decode('utf-8')
+    array = (msg.rstrip()).split(":") # retire la nouvelle ligne puis sépare l'identifiant de la valeur
+    if array[0] == "TEMP":
+        print(array[1] + "°C")
+    elif array[0] == "HUMI":
+        print(array[1] + "%")
+    elif array[0] == "MOIS":
+        print(array[1] + " (sans unitée)")
 
 ser.close()     

a7b2597351e9ff3099726b6d882451f05155bfe2
Constantin Bouis <cbouis@outlook.fr>
2020-01-18 17:17:28 -0500

    alex lencule

--- webapp/uno.py
+++ webapp/uno.py
@@ -8,14 +8,14 @@ def handle_signals():
     check_temperature()
 
 def check_water():
-    water_level = READUNO()
-    if water_level<const.WATER_TH:
-        en_gpio(3000,const.GPIO_PUMP) #Should be calculated depending on the uno value
+    #water_level = READUNO()
+    #if water_level<const.WATER_TH:
+    en_gpio(3000,const.GPIO_PUMP) #Should be calculated depending on the uno value
 
 def check_temperature():
-    temperature_level = READUNO()
-    if temperature_level<const.TEMPERATURE_TH:
-        en_gpio(3000,const.GPIO_FAN) #Should be calculated depending on the uno value
+    #temperature_level = READUNO()
+    #if temperature_level<const.TEMPERATURE_TH:
+    en_gpio(3000,const.GPIO_FAN) #Should be calculated depending on the uno value
 
 def en_gpio(t, gpio_port):
     gpio.turn_on(gpio_port)

4eb4ec6b00a0b36deeb64f467a9870b99ecd4f4f
Constantin Bouis <cbouis@outlook.fr>
2020-01-18 16:53:28 -0500

    ntm

--- led.py
+++ led.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_NO, GPIO.OUT)
+
 def turn_on(GPIO_NO):
-	
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(GPIO_NO, GPIO.OUT)
 
 	GPIO.output(GPIO_NO,True)
 
+def turn_off(GPIO_NO):
+
+	GPIO.output(GPIO_NO,False)
 
 turn_on(23)
+sleep(1)
+turn_off(23)

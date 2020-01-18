06d0f8501c8485deb045e72507b695fc9970fb94
Constantin Bouis <cbouis@outlook.fr>
2020-01-18 18:37:34 -0500

    on fait des bails pour read uno oklm

--- webapp/uno.py
+++ webapp/uno.py
@@ -2,7 +2,7 @@
 import constants as const
 import gpio as gpio
 import time
-import serial    
+from readSerial import read    
 
 
 def handle_signals():
@@ -10,12 +10,12 @@ def handle_signals():
     check_temperature()
 
 def check_water():
-    water_level = read_uno()
+    water_level = 200
     if water_level<const.WATER_TH:
         en_gpio(3,const.GPIO_PUMP) #Should be calculated depending on the uno value
 
 def check_temperature():
-    temperature_level = READUNO()
+    temperature_level = 200
     if temperature_level<const.TEMPERATURE_TH:
         en_gpio(3,const.GPIO_FAN) #Should be calculated depending on the uno value
 
@@ -24,16 +24,5 @@ def en_gpio(t, gpio_port):
     time.sleep(t)
     gpio.turn_off(gpio_port)
 
-def read_uno():
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5) # open serial port with 5s timeout
-    while(1):
-        msg = ser.readline().decode('utf-8')
-        array = (msg.rstrip()).split(":") # retire la nouvelle ligne puis separe l'identifiant de la valeur
-    if array[0] == "TEMP":
-        print(array[1] + "C")
-    elif array[0] == "HUMI":
-        print(array[1] + "%")
-    elif array[0] == "MOIS":
-        print(array[1] + " (sans unitee)")
-
-    ser.close()    
+#def read_uno():
+   # serial.read()
